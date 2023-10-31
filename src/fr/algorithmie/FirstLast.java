1e1752a750823d738abcd3aee962313d25cc8820
rijarandria <rijandrisolo@gmail.com>
2023-10-31 16:13:46 +0100

    Ajout FirstLast6.java et FirstLast.java

---
+++ src/fr/algorithmie/FirstLast.java
@@ -0,0 +1,17 @@
+package fr.algorithmie;
+
+public class FirstLast {
+    public static void main(String[] args) {
+        //int[] array = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8} ;
+        //int[] array = new int[0];
+        int[] array = {6, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 6} ;
+        //int[] array = {1, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 6} ;
+        boolean val = false;
+        if(array.length>=1 && (array[0]==6 && array[array.length-1]==6)){
+            val=true;
+        }
+
+
+        System.out.println(val);
+    }
+}
