67c99a0c1c7118bbf714ab99d511994758bdefbd
rijarandria <rijandrisolo@gmail.com>
2023-10-31 15:23:39 +0100

    Ajout ComparaisonTableau.java

---
+++ src/fr/algorithmie/ComparaisonTableau.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class ComparaisonTableau {
+    public static void main(String[] args) {
+        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4} ;
+        int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8} ;
+        int k = 0;
+        for(int i=0;i <=array2.length;i++) {
+
+                    for (int j = 0; j <= array2.length-1; j++) {
+                        if (array1[i] == array2[j]) {
+                            k++;
+
+                        }
+                    }
+
+        }
+        System.out.println( k +" Eléments en commun");
+    }
+}
