1b814f7f8fc1a68d250a727745ac788b1cecf835
rijarandria <rijandrisolo@gmail.com>
2023-11-04 12:21:05 +0100

    Ajout do while dans Multiplication.java

--- src/fr/algorithmie/Multiplication.java
+++ src/fr/algorithmie/Multiplication.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 public class Multiplication {
     public static void main(String[] args) {
 
-        try ( Scanner scanner = new Scanner( System.in )){
+        int chiffre ;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print( "Veuillez saisir un entier entre 1 et 10 : " );
+             chiffre = scanner.nextInt();
 
-        System.out.print( "Veuillez saisir un entier : " );
-        int chiffre = scanner.nextInt();
-
-        if(chiffre>=1 && chiffre <= 10) {
-            for (int i = 0; i <= 10; i++) {
-                System.out.println(chiffre + " * " + i + " = " + chiffre * i);
+            if(chiffre>=1 && chiffre <= 10) {
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println(chiffre + " * " + i + " = " + chiffre * i);
+                }
+            }else{
+                System.out.println("Chiffre invalide");
             }
-        }else{
-            System.out.println("Chiffre invalide");
-        }
-    }
+
+        }while (chiffre > 0 && chiffre <= 10) ;
+
+
     }
 }
+
